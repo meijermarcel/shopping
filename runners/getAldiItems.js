7a48f1059d7be533f553dd13b91872a6b9248b3a
meijermarcel <47677900+meijermarcel@users.noreply.github.com>
2024-06-09 09:57:15 -0400

    feat: adds request pagination to load all data

--- runners/getAldiItems.js
+++ runners/getAldiItems.js
@@ -1,19 +1,27 @@
 const axios = require('axios');
-// const cheerio = require('cheerio');
-const dayjs = require('dayjs')
+const dayjs = require('dayjs');
 
 if (process.env.NODE_ENV !== 'production') {
-    // require('dotenv').load();
     require('dotenv').config({ path: require('find-config')('.env') });
 }
 
 const { StoreItem } = require('../models/storeItem');
 const { StoreItemPrice } = require('../models/storeItemPrice');
 
-function getAldiItems(category_key) {
-    const url = `https://api.aldi.us/v1/catalog-search-product-offers?currency=USD&serviceType=pickup&page%5Blimit%5D=24&page%5Boffset%5D=0&sort=relevance&category_key=${category_key}&merchantReference=467-108`
-    return axios.get(url).then(async res => {
+async function getAldiItems(category_key) {
+    let maxPage = 1;
+    let currentPage = 0;
+    const itemsPerPage = 48;
+    while (currentPage < maxPage) {
+        const url = `https://api.aldi.us/v1/catalog-search-product-offers?currency=USD&serviceType=pickup&page%5Blimit%5D=${itemsPerPage}&page%5Boffset%5D=${
+            currentPage * itemsPerPage
+        }&sort=relevance&category_key=${category_key}&merchantReference=467-108`;
+        const res = await axios.get(url);
+
         const results = res.data.data[0].attributes.catalogSearchProductOfferResults;
+        const pagination = res.data.data[0].attributes.pagination;
+        currentPage = pagination.currentPage;
+        maxPage = pagination.maxPage;
         for (let result of results) {
             const nameSplit = result.name.split(',');
             const description = nameSplit[0];
@@ -21,8 +29,8 @@ function getAldiItems(category_key) {
             const price = result.prices[0].formattedPrice.slice(1);
             const contentUnit = result.contentUnit;
             const netContent = result.netContent;
-            const imageLink = result.images[0].externalUrlLarge;
-            console.log(result.name, price);
+
+            const imageLink = result.images && result.images.length ? result.images[0].externalUrlLarge : '';
             const existingStoreItem = await StoreItem.findOne({
                 where: {
                     description
@@ -50,11 +58,8 @@ function getAldiItems(category_key) {
                     dateTime: dayjs().startOf('day').format()
                 });
             }
-
-            console.log(existingStoreItem);
         }
-    }).catch(err => console.log(err));
-
+    }
 }
 
 // Fresh Fruit
