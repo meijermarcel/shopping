275bc176dd862c425aa602c91422de994b90eb50
Marcel Meijer <47677900+meijermarcel@users.noreply.github.com>
2024-08-15 19:20:33 -0400

    feat: add individual item route

--- controllers/aldiController.js
+++ controllers/aldiController.js
@@ -3,10 +3,22 @@ const { StoreItemPrice } = require('../models/storeItemPrice');
 
 const init = (router) => {
     router.get('/aldi/items', async (req, res) => {
-        const items = await StoreItem.findAll({
-            include: StoreItemPrice
-        });
+        const items = await StoreItem.findAll();
         res.send(items);
+    });
+
+    router.get('/aldi/item/:id', async (req, res) => {
+        const item = await StoreItem.findOne({
+            include: [
+                {
+                    model: StoreItemPrice
+                }
+            ],
+            where: {
+                id: req.params.id
+            }
+        });
+        res.send(item);
     });
 };
 
